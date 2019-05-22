f0ba1596439073aca02d04471f8269e5284d4f1a
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-22 19:39:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var theBeatles = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     theBeatles.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return theBeatles;
