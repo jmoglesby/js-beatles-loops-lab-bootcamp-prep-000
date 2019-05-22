f34a1fec80a5e8ced438921b09b9e86fe67c06ee
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-22 19:38:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var theBeatles = [];
+  for (i = 0; i < musicians.length; i++) {
+    myList.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+}
