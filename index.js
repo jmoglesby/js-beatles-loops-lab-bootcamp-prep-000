a126fa6239c0fc989ecc9351b1f264fb0306ec63
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-22 19:49:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function johnLennonFacts(facts) {
   var lennonFacts = [];
   while (counter > 0) {
     var fact = facts[counter - 1];
-    fact = `${fact}!!!`;
-    lennonFacts.push(fact);
+    lennonFacts.push(`${fact}!!!`);
     counter--;
   }
   return lennonFacts;
